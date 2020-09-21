1c476486db214d4bfb4c98f5c54d25d66f2423e3
André Lima <andredlima23@gmail.com>
2020-09-21 13:29:30 -0300

    Criação do componente delete
    Criação do componente delete
    cadastro do novo produto, mas ainda não consigo excluir

--- crudC10P/frontend/src/app/app-routing.module.ts
+++ crudC10P/frontend/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component'; 
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductsDeleteComponent } from './components/product/products-delete/products-delete.component';
 
 const routes: Routes = [{
   path: "",
@@ -21,6 +22,10 @@ const routes: Routes = [{
 {
   path: "products/update/:id",
   component: ProductUpdateComponent
+},
+{
+  path: "products/delete/:id",
+  component: ProductsDeleteComponent
 }];
 
 @NgModule({
